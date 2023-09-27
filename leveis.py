2c39332a5329a4f94e1b45179c64000565bf253b
JLSF2 <139147743+JLSF2@users.noreply.github.com>
2023-09-27 13:09:51 -0300

    Mapa 1, Mapa 4

--- leveis.py
+++ leveis.py
@@ -1,28 +1,20 @@
 from classes import Zumbi,Jogador,Pizza,Coca_cafe
 from main import *
-#zumbies/pizzas/coca/player
+
+def cria_Paredes(grid):
+        Paredes = []
+        x=y=0
+        for linha in grid:
+            for coluna in linha:
+                if coluna =='P':
+                    Paredes.append(Parede((x,y)))
+                x+=32
+            y+=32
+            x=0
+        return Paredes
 
 
-zumbi_1_1 = Zumbi(100, 30, 10,
-               movimento_y=True)
-zumbi_1_2 = Zumbi(300, 30, 10,
-               movimento_y=True)
-zumbi_1_3 = Zumbi(400, 30, 10,
-               movimento_y=True)
-
-
-
-jogador_1 = Jogador(640 // 2, 480 // 2, 10)  # x_inicial, y_inicial, velocidade
-
- 
-# Pizzas e Coca café
-pizza_1_1 = Pizza(400, 300)  # x_inicial, y_inicial, coletada = False
-pizza_1_2 = Pizza(400, 100)  # x_inicial, y_inicial, coletada = False
-coca_1 = Coca_cafe(50, 100)  # x_inicial, y_inicial, coletada = False
-
-leveit=[[zumbi_1_1,zumbi_1_2,zumbi_1_3],[pizza_1_1,pizza_1_2],[coca_1],[jogador_1]]
-
-print(zumbi_1_1.cor)
+Levels = []
 
 grid_1=[
     'PPPPPPPPPPPPPPPPPPPP',
@@ -39,8 +31,20 @@ grid_1=[
     'P-P---P------P---P-P',
     'P-PPPPPPP--PPPPPPP-P',
     'P------------------P',
-    'PPPPPPPPPPPPPPPPPPPP'  
+    'PPPPPPPPPPPPPPPPPPPP'
 ]
+porta1 = Porta(18*32,32*1)
+Paredes1 = cria_Paredes(grid_1)
+jogador_coord_init1 = [32*1, 13*32]  #x_inicial, y_inicial,
+jogador1 = Jogador(jogador_coord_init1, 10, porta1, Paredes1)  # coordenadas e velocidade
+
+#A ordem dos zumbis vão de cima p/ baixo no mapa 1 - o Segundo é o zumbi no canto
+Zumbis1 = [Zumbi(32, 32, 10, Paredes1, movimento_x=True),Zumbi(18*32, 13*32, 10, Paredes1, movimento_y=True),Zumbi(32*6, 9*32, 10, Paredes1, movimento_y=True,direcao_y = -1),Zumbi(14*32, 5*32, 10, Paredes1, movimento_y=True),Zumbi(11*32, 6*32, 10, Paredes1, movimento_y=True),Zumbi(12*32, 11*32, 10, Paredes1, movimento_x=True,direcao_x=-1)]
+
+Pizzas1 = [Pizza(9*32+8, 3*32+8,),Pizza(5*32+8, 5*32+8),Pizza(12*32+8, 10*32+8)]
+cracha1 = Cracha(10*32+8, 6*32+8)
+Mapa1 = [grid_1, Paredes1, porta1, jogador1, Zumbis1, Pizzas1, cracha1]
+Levels.append(Mapa1)
 
 grid_2=[
     'PPPPPPPPPPPPPPPPPPPP',
@@ -57,8 +61,9 @@ grid_2=[
     'P--PP--PP--PP--PP--P',
     'P--PP--PP--PP--PP--P',
     'P------------------P',
-    'PPPPPPPPPPPPPPPPPPPP'  
+    'PPPPPPPPPPPPPPPPPPPP'
 ]
+
 
 grid_3=[
     'PPPPPPPPPPPPPPPPPPPP',
@@ -75,8 +80,9 @@ grid_3=[
     'P------P---------P-P',
     'P------PPPPPPPPPPP-P',
     'P------------------P',
-    'PPPPPPPPPPPPPPPPPPPP'  
+    'PPPPPPPPPPPPPPPPPPPP'
 ]
+
 
 #Esse aqui é o de Ismael
 grid_4=[
@@ -96,6 +102,16 @@ grid_4=[
     'P--P-PP---P---P--P--',
     'PPPPPPPPPPPPPPPPPPPP'
 ]
+porta4 = Porta(608,416)
+Paredes4 = cria_Paredes(grid_4)
+jogador_coord_init4 = [64, 416]  #x_inicial, y_inicial,
+jogador4 = Jogador(jogador_coord_init4, 10, porta4, Paredes4)  # coordenadas e velocidade
+Zumbis4 = [Zumbi(32, 64, 10, Paredes4, movimento_y=True),Zumbi(200, 32, 10, Paredes4, movimento_x=True),Zumbi(32, 400, 10, Paredes4, movimento_y=True,direcao_y = -1),Zumbi(224, 352, 10, Paredes4, movimento_x=True),Zumbi(384, 224, 10, Paredes4, movimento_y=True),Zumbi(160, 224, 10, Paredes4, movimento_y=True),Zumbi(512, 256, 10, Paredes4, movimento_x=True),Zumbi(512, 384, 10, Paredes4, movimento_x=True),Zumbi(544, 128, 10, Paredes4, movimento_x=True)]
+Pizzas4 = [Pizza(480+8, 64+8),Pizza(320+8, 160+8),Pizza(128+8, 416+8)]
+Cocas4 = [Coca_cafe(288+8, 288+8)]
+cracha4 = Cracha(192+8, 32+8)
+Mapa4 = [Paredes4, porta4, jogador4, Zumbis4, Pizzas4, Cocas4, cracha4]
+Levels.append(Mapa4)
 
 #Tá vazio pq não pensei em nada bom ainda
 grid_5=[
@@ -113,5 +129,22 @@ grid_5=[
     'P------------------P',
     'P------------------P',
     'P------------------P',
-    'PPPPPPPPPPPPPPPPPPPP'  
+    'PPPPPPPPPPPPPPPPPPPP'
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from classes import*
+from leveis import Levels
 
 
 PRETO = (0, 0, 0)
@@ -9,17 +10,18 @@ CINZA = (150, 150, 150)
 
 pygame.init()
 LARGURA, ALTURA = 640, 480
-
+porta = Porta(608,416)
 # Player
 jogador_coord_init = [64, 416]  #x_inicial, y_inicial,
-jogador = Jogador(jogador_coord_init, 10)  # coordenadas e velocidade
+jogador = Jogador(jogador_coord_init, 10, porta)  # coordenadas e velocidade
 
 # Zumbis
 Zumbis = [Zumbi(32, 64, 10,movimento_y=True),Zumbi(200, 32, 10,movimento_x=True),Zumbi(32, 400, 10,movimento_y=True,direcao_y = -1),Zumbi(224, 352, 10,movimento_x=True),Zumbi(384, 224, 10,movimento_y=True),Zumbi(160, 224, 10,movimento_y=True),Zumbi(512, 256, 10,movimento_x=True),Zumbi(512, 384, 10,movimento_x=True),Zumbi(544, 128, 10,movimento_x=True)]
 
-# Pizzas e Coca café
+# Pizzas, Coca café e Crachá
 Pizzas = [Pizza(480+8, 64+8),Pizza(320+8, 160+8),Pizza(128+8, 416+8)]
 Cocas = [Coca_cafe(288+8, 288+8)]
+cracha = Cracha(192+8, 32+8)
 
 
 # Define o título da JANELA e estabelece a taxa de quadros por segundo.
@@ -43,21 +45,25 @@ def colisoes(pizzas_possuidas):
             else:
                 jogador.vidas -= 1
                 for zumbi in Zumbis:
-                    zumbi.velocidade = zumbi.velocidade_base 
-                jogador.rect.x, jogador.rect.y = jogador_coord_init[0], jogador_coord_init[1]  # Retorna o jogador a posição predefinida.
+                    zumbi.velocidade = zumbi.velocidade_base
+                jogador.rect.x, jogador.rect.y = jogador.x_inicial, jogador.y_inicial # Retorna o jogador a posição predefinida.
     #Colisão entre pizza e jogador
     for pizza in Pizzas:
         if jogador.rect.colliderect(pizza.rect) and not pizza.coletada:
             pizza.coletada = True  # Faz a Pizza desaparecer e não poder ser coletada mais vezes
             pizzas_possuidas += 1
     # Colisão entre coca café e jogador. Faz os zumbis se moverem com metade da velocidade pelos próximos 10 segundos.
-    for coca_cafe in Cocas:
-        if jogador.rect.colliderect(coca_cafe.rect) and not coca_cafe.coletada:
-            coca_cafe.coletada = True  # Faz a coca_cafe desaparecer e não poder ser coletada mais vezes
-            for zumbi in Zumbis:
-                zumbi.velocidade *= 1/2
-            FIM_DO_BONUS = pygame.USEREVENT + 1
-            pygame.time.set_timer(FIM_DO_BONUS, 10000, 1)
+    if len(Cocas) != 0:
+        for coca_cafe in Cocas:
+            if jogador.rect.colliderect(coca_cafe.rect) and not coca_cafe.coletada:
+                coca_cafe.coletada = True  # Faz a coca_cafe desaparecer e não poder ser coletada mais vezes
+                for zumbi in Zumbis:
+                    zumbi.velocidade *= 1/2
+                FIM_DO_BONUS = pygame.USEREVENT + 1
+                pygame.time.set_timer(FIM_DO_BONUS, 10000, 1)
+    if jogador.rect.colliderect(cracha.rect) and not cracha.coletada:
+            cracha.coletada = True  # Faz o crachá desaparecer e não poder ser coletada mais vezes
+            jogador.tem_cracha = True
     return pizzas_possuidas
 
 
@@ -67,6 +73,8 @@ def reiniciar():
     Jogador.cor = BRANCO
     jogador.vidas = 3
     jogador.velocidade = 10
+    jogador.tem_cracha = False
+    cracha.coletada = False
     for zumbi in Zumbis:
         zumbi.rect.x, zumbi.rect.y = zumbi.x_inicial, zumbi.y_inicial
     for pizza in Pizzas:
@@ -77,23 +85,34 @@ def reiniciar():
 
 
 def rodar_jogo():
-    fim_jogo = False
+    continuar = True
+    level_atual = 0
+    while continuar:
+        Paredes = Levels[level_atual][0]
+        porta = Levels[level_atual][1]
+        jogador = Levels[level_atual][2]
+        Zumbis = Levels[level_atual][3]
+        Pizzas = Levels[level_atual][4]
+        Cocas = Levels[level_atual][5]
+
+
+    fim_de_nivel = False
     pizzas_possuidas = 0
-    while not fim_jogo:
+    while not fim_de_nivel:
         fonte_contador = pygame.font.Font(None, 20)
-        texto_contador = fonte_contador.render("Pizza:" + str(pizzas_possuidas) + "       Vidas:" + str(jogador.vidas), True, BRANCO)
+        texto_contador = fonte_contador.render("Pizza:" + str(pizzas_possuidas) + "       Vidas:" + str(jogador.vidas) + '         Crachá:' + str(jogador.tem_cracha), True, BRANCO)
         pygame.time.delay(50)
         relogio.tick(FPS)
         # Condição de interromper código
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                fim_jogo = True
+                fim_de_nivel = True
             if evento.type == pygame.USEREVENT + 0:
                 jogador.invulnerabilidade = False
                 Jogador.cor = BRANCO
             if evento.type == pygame.USEREVENT + 1:
                 for zumbi in Zumbis:
-                    zumbi.velocidade = zumbi.velocidade_base 
+                    zumbi.velocidade = zumbi.velocidade_base
         # Controla o movimento do jogador, dos Zumbis, testa colisões e avalia se o jogo terminou. Então, redesenha a janela de acordo.
         comandos = pygame.key.get_pressed()
         jogador.movimento(comandos, LARGURA, ALTURA)
@@ -102,21 +121,27 @@ def rodar_jogo():
         pizzas_possuidas = colisoes(pizzas_possuidas)
         if jogador.vidas == 0:
             pizzas_possuidas = reiniciar()
+        if jogador.passou_de_fase:
+            level_atual += 1
 
         JANELA.fill(PRETO)
-        for parede in Parede.paredes:
+        for parede in Paredes:
             pygame.draw.rect(JANELA,(80,9,200), parede.rect)
 
         pygame.draw.rect(JANELA, (Jogador.cor), jogador.rect)  # JANELA, cor, tamanho
         for zumbi in Zumbis:
             pygame.draw.rect(JANELA, Zumbi.cor, zumbi.rect)  # quadrado verde-zumbi
-        for coca_cafe in Cocas:
-            if not coca_cafe.coletada:
-                pygame.draw.rect(JANELA, Coca_cafe.cor, coca_cafe.rect)  # retangulo marrom
+        if len(Cocas) != 0:
+            for coca_cafe in Cocas:
+                if not coca_cafe.coletada:
+                    pygame.draw.rect(JANELA, Coca_cafe.cor, coca_cafe.rect)  # retangulo marrom
         for pizza in Pizzas:
             if not pizza.coletada:
                 pygame.draw.rect(JANELA, Pizza.cor, pizza.rect)  # retângulo pizza
-        JANELA.blit(texto_contador, [32, 32])   
+        if not cracha.coletada:
+            pygame.draw.rect(JANELA,Cracha.cor, cracha.rect)
+        pygame.draw.rect(JANELA, Porta.cor, porta.rect)
+        JANELA.blit(texto_contador, [32, 32])
         pygame.display.update()
 
 
