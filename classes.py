7c4541e06c97ad9daecbda10344d332f10cd40bc
Sara Pereira <124205616+SaraEmilay@users.noreply.github.com>
2023-09-27 22:01:06 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import pygame
-
+from main import*
+pygame.init()
 #Classe do Jogador, com único método movimento e movimento linear.
 class Jogador:
     cor = (255, 255, 255)
@@ -132,6 +133,44 @@ class Cracha:
 
 #Classe da saida, ainda faltam informações quanto ao tamanho e tals
 class Porta:
+
     cor = (255, 0, 0)
     def __init__(self, x, y):
         self.rect = pygame.Rect(x, y, 32, 32)
+        
+LARGURA, ALTURA = 640, 480
+JANELA = pygame.display.set_mode((LARGURA, ALTURA))
+fonte_textos=pygame.font.SysFont('arial', 30)
+#classe dos botões
+class Botao:
+
+    def __init__ (self, texto,altura,largura,x,y):
+        self.rect=pygame.Rect(x,y,altura,largura)
+        self.cor_botao=255,255,255
+
+        self.pressionado=False
+
+        self.texto_flutuante= fonte_textos.render(texto,True,(0,0,0))
+        self.texto_rect=self.texto_flutuante.get_rect(center=self.rect.center)
+
+    def draw(self):
+        pygame.draw.rect(JANELA,(255,255,255),self.rect,border_radius=12)
+        JANELA.blit(self.texto_flutuante,self.texto_rect)
+        return self.click_verif()
+
+    def click_verif(self):
+        self.click=False
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            self.cor_botao=0,8,9
+            if pygame.mouse.get_pressed()[0]:
+                self.pressionado=True
+            else:
+                if self.pressionado==True:
+                    self.click=True
+
+                    self.pressionado=False                   
+        else:
+            self.cor_botao=0,200,49
+        return self.click
+    
